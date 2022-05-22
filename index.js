d525eb0ca019cb6ed922257c573a1f99ce6a5e4d
H_A _Opu <wwwhasib4@gmail.com>
2022-05-22 17:00:26 +0600

    making simple run function and connecting mongodb

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pme8g.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    client.connect();
+    console.log("making run arrow function");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Automotive Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Automotive app listening on Port ${port}`);
+});
